cd90b8f66c171eadfb18f7d321e1599020e1fc83
Gregorie Calderon <calderon.greg6112@gmail.com>
2021-04-28 01:46:46 -0700

    Implemented skills section

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AboutMeHeader from './AboutMeHeader.jsx';
 import AboutMeContent from './AboutMeContent.jsx';
+import AboutMeSkills from './AboutMeSkills.jsx';
 
 const AboutMe = () => (
   <div className="AboutMeSection" id="AboutMeSection">
     <AboutMeHeader />
     <AboutMeContent />
+    <AboutMeSkills />
   </div>
 );
 export default AboutMe;
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -14,10 +14,13 @@ class App extends React.Component {
     let title = document.getElementsByClassName('WorkCompleted');
     let aboutMe = document.getElementsByClassName('AboutMePicture');
     let aboutMeText = document.getElementsByClassName('AboutMeInformationText');
+    let aboutMeTech = document.getElementsByClassName('AboutMeSkills');
+    let arr5 = [...aboutMeTech];
     let arr4 = [...aboutMeText];
     let arr3 = [...aboutMe];
     let arr2 = [...title];
     let arr = [...projects];
+    showScroll(arr5);
     showScroll(arr4);
     showScroll(arr3);
     showScroll(arr2);
