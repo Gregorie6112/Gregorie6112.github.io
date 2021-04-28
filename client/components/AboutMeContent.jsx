e5e510cc6f997c7c6663d97338a93906e3d3ba44
Gregorie Calderon <calderon.greg6112@gmail.com>
2021-04-27 23:55:45 -0700

    Made projects.js file

---
+++ client/components/AboutMeContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tilt from 'react-vanilla-tilt';
+
+const AboutMeContent = () => (
+  <div className="AboutMeContent">
+    <div className="AboutMePicture">
+        <img className="AboutMePic" src="https://cdn.discordapp.com/attachments/550198988704579654/816156728126472202/Screen_Shot_2021-03-01_at_7.55.03_PM.png"></img>
+    </div>
+    <div className="AboutMeInformation"></div>
+  </div>
+);
+export default AboutMeContent;
--- client/components/ProjectsDisplay.jsx
+++ client/components/ProjectsDisplay.jsx
@@ -1,45 +1,7 @@
 import React from 'react';
 import Tilt from 'react-vanilla-tilt';
 import GeneralInformation from './GeneralInfo.jsx';
-
-let projects = [
-  {
-    projectTechStack: "React, Javascript, Jest, Github Actions, Node.js, Express, MongoDB",
-    projectSelfProject: "Yes",
-    projectName: "Algorithm Visualizer",
-    projectImage: "https://images.unsplash.com/photo-1585076641399-5c06d1b3365f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-    projectCode: "https://github.com/Gregorie6112/datastructures",
-    projectWebsite: "",
-    projectGeneralInfo: "My main purpose for building this application was due to my gap in knowledge with data structures, algorithms and the whole development life cycle. I recognized my weak spots and decided to improve on it by building an application that would visualize the algorithms working. Utilized Figma for component building, Continuous Integration, Jest unit testing, and currently working on refactoring to use React Hooks."
-  },
-  {
-    projectTechStack: "React, Javascript, MongoDB, Node.js, Express",
-    projectSelfProject: "Yes",
-    projectName: "Stock Data Visualization",
-    projectImage: "https://images.unsplash.com/photo-1613843433065-819a04a47a09?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
-    projectCode: "https://github.com/Gregorie6112/Chartify",
-    projectWebsite: "https://chartify-gregorie-calderon.herokuapp.com/",
-    projectGeneralInfo: "From Design to Implementation, the main goal of the project was to visually render stock data fetched using Axios and storing the information into the database to allow the user to re search for a new ticker and if need be , navigate back to previous ticker symbol that was searched without submitting a new request. If a new request was submitted for the same ticker, it would update the database with the new most up to date price."
-  },
-  {
-    projectTechStack: "React, Javascript, postgreSQL / MongoDB, Node.js, Express, k6, New Relic, Loader.io",
-    projectSelfProject: "No",
-    projectName: "Database Optimization",
-    projectImage: "https://images.unsplash.com/photo-1520263115673-610416f52ab6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-    projectCode: "https://github.com/NikeSDC/relatedProducts",
-    projectWebsite: "https://docs.google.com/document/d/1XyOY2WJpS-u0o08tnVC8YqyUWa42yzIO_61Pyj6kCj0/edit?usp=sharing",
-    projectGeneralInfo: "Lead 7 engineers while inheriting legacy code base and refactored from a non-relational to relational. Optimize the backend queries and database to withstand a sudden influx of users x1000 RPS and locally test existing infrastructure before deploying to AWS and using Loader.io to stress test in the cloud while using NGINX load balancers.",
-  },
-  {
-    projectTechStack: "React, Javascript, postgreSQL, Node.js, Express, Socket.io, AWS, Firebase, Bootstrap",
-    projectSelfProject: "No",
-    projectName: "Synchronous Chat Application",
-    projectImage: "https://images.unsplash.com/photo-1611606063065-ee7946f0787a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
-    projectCode: "https://github.com/hrla41-mvp/main-app",
-    projectWebsite: "http://13.52.214.136:3000/",
-    projectGeneralInfo: "Lead a team of 4 engineers, including myself to acheive a real time chat application. From distributing work based on career goals to pair-programming and code reviews, the goal was to get this application out in 2 weeks deployed and ready to use. Backend built on postgreSQL and Frontend is React.js."
-  },
-];
+import projects from '../projects.js';
 
 const ProjectsDisplay = () => (
   <div className="ProjectsDisplay">
