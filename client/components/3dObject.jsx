e7f9e9be5c9b20f0e9691d6c30fb102670c0d570
Gregorie Calderon <calderon.greg6112@gmail.com>
2021-04-29 02:09:32 -0700

    Implemented new header , still testing

--- client/components/3dObject.jsx
+++ client/components/3dObject.jsx
@@ -4,7 +4,7 @@ import Name3dObject from './name3dObject.jsx';
 const ThreeDObject = () => (
   <>
   <Name3dObject />
-  <iframe src='https://my.spline.design/untitled-dcff325b2c4af2ca8d24898b86852fb0/' id="Home" frameBorder='0' width='100%' height='100%'></iframe>
+  <iframe src='/Header' id="Home" frameBorder='0' width='100%' height='100%'></iframe>
   </>
 );
 export default ThreeDObject;
